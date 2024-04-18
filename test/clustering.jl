eb44b28f6e54555c433d1db5185f16e792b01113
Xuanzhao Gao <gaoxuanzhao@gmail.com>
2024-04-18 19:59:18 +0800

    update tests

--- test/clustering.jl
+++ test/clustering.jl
@@ -1,12 +1,13 @@
 using Test, TensorBranching
 
 @testset "gathering algorithm" begin
-    N = 30
-    bs = [rand(BitStr{10, Int64}) for i in 1:30]
+    n = 30
+    m = 10
+    bs = [rand(BitStr{m, Int64}) for i in 1:n]
     ca = Clause.(bs)
     cca = copy(ca)
-    for _=1:N - 1
+    for _=1:n - 1
         cca = gather(cca)
-        @test foldl(.|, [covered_by(bs, ccai) for ccai in cca]; init = [false for i in 1:N]) == ones(N)
+        @test foldl(.|, [covered_by(bs, ccai) for ccai in cca]; init = [false for i in 1:n]) == ones(n)
     end
 end
