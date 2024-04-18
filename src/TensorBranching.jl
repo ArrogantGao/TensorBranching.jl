afc80cc3849bdf56522434bf582191d16d80bf98
Xuanzhao Gao <gaoxuanzhao@gmail.com>
2024-04-18 20:52:42 +0800

    test lol commit

--- src/TensorBranching.jl
+++ src/TensorBranching.jl
@@ -13,6 +13,7 @@ export sbranches, complexity
 export graph_from_tuples, reduced_alpha, reduced_alpha_configs, collect_configs, BranchingTable, DNF, booleans, covered_by
 export missolve
 
+
 include("bitstring.jl")
 include("clustering.jl")
 include("setcovering.jl")
