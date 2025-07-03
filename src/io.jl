4a3ef478f1a00e2d90a230b2abe8d4458eecbc86
Xuanzhao Gao <gaoxuanzhao@gmail.com>
2025-07-04 01:02:20 +0800

    revise tests

--- src/io.jl
+++ src/io.jl
@@ -7,6 +7,10 @@ function save_weights(filename::String, weights::UnitWeight)
 end
 
 function save_weights(filename::String, weights::Vector{T}) where T
+    # if f ex
+    if isfile(filename)
+        rm(filename)
+    end
     f = open(filename, "w")
     write(f, "$(T)\n")
     for w in weights
@@ -24,15 +28,19 @@ function load_weights(filename::String)
         else
             if line1 == "Float64"
                 T = Float64
+            elseif line1 == "Float32"
+                T = Float32
             elseif line1 == "Int64"
                 T = Int64
+            elseif line1 == "Int32"
+                T = Int32
             else
                 error("Unknown weight type: $line1")
             end
-            return [parse(T, line) for line in eachline(f)]
+            weights = [parse(T, line) for line in eachline(f)]
+            return weights
         end
     end
-    return nothing
 end
 
 function threaded_saveslices(dirname::String, slices::Vector{SlicedBranch}, ids::Vector{Int})
@@ -60,13 +68,14 @@ function save_code(filename::String, code::Nothing)
 end
 
 function load_code(filename::String)
-    open(filename, "r") do f
-        line1 = readline(f)
-        if line1 == "nothing"
-            return nothing
-        end
+    f = open(filename, "r")
+    line1 = readline(f)
+    if line1 == "nothing"
+        return nothing
+    else
+        close(f)
+        return readjson(filename)
     end
-    return readjson(filename)
 end
 
 function save_finished(dirname::String, branch::SlicedBranch, id::Int)
@@ -79,6 +88,16 @@ function save_finished(dirname::String, branch::SlicedBranch, id::Int)
     return nothing
 end
 
+function load_finished(dirname::String, id::Int)
+    graph_name = joinpath(dirname, "graph_$(id).dot")
+    code_name = joinpath(dirname, "eincode_$(id).json")
+    weights_name = joinpath(dirname, "weights_$id.txt")
+    g = loadgraph(graph_name)
+    code = load_code(code_name)
+    weights = load_weights(weights_name)
+    return g, code, weights
+end
+
 function save_unfinished(dirname::String, branch::SlicedBranch, id::Int)
     filename = joinpath(dirname, "unfinished_$(id).jld")
     @save filename branch
@@ -89,4 +108,14 @@ function load_unfinished(dirname::String, id::Int)
     filename = joinpath(dirname, "unfinished_$(id).jld")
     branch = load(filename, "branch")
     return branch
+end
+
+function load_all_finished(dirname::String)
+    df = CSV.read(joinpath(dirname, "slices.csv"), DataFrame)
+    branches = SlicedBranch[]
+    for id in df.id
+        g, code, weights = load_finished(dirname, id)
+        push!(branches, SlicedBranch(MISProblem(g, weights), code, df.r[df.id .== id][1]))
+    end
+    return branches
 end
--- test/kernelize.jl
+++ test/kernelize.jl
@@ -11,21 +11,15 @@ Random.seed!(1234)
 @testset "kernelize" begin
     for n in 30:10:60
         g = random_regular_graph(n, 3)
-        for reducer in [MISReducer(), XiaoReducer(), TensorNetworkReducer()]
-            g_new, r, vmap, reducer_new = kernelize(g, reducer)
-            mis_1 = mis2(EliminateGraph(g_new)) + r
-            mis_2 = mis2(EliminateGraph(g))
-            @test nv(g_new) ≤ nv(g)
-            @test mis_1 == mis_2
+        for ws in [UnitWeight(n), Float32.(1.0 .+ rand(n))]
+            for reducer in [BasicReducer(), XiaoReducer(), TensorNetworkReducer()]
+                @info "n = $n, ws = $ws, reducer = $reducer"
+                res = kernelize(g, ws, reducer)
+                mis_1 = solve(GenericTensorNetwork(IndependentSet(res.g, res.weights), optimizer = TreeSA()), SizeMax(), T = Float32)[].n + res.r
+                mis_2 = solve(GenericTensorNetwork(IndependentSet(g, ws), optimizer = TreeSA()), SizeMax(), T = Float32)[].n
+                @test nv(res.g) ≤ nv(g)
+                @test mis_1 ≈ mis_2
+            end
         end
-    end
-
-    for g in [random_regular_graph(300, 3), SimpleGraph(GenericTensorNetworks.random_diagonal_coupled_graph(40, 40, 0.8))]
-        reducer = TensorNetworkReducer()
-        g_new, _, _, reducer_new = kernelize(g, reducer)
-        g_new_new, _, _, reducer_new_new = kernelize(g_new, reducer_new)
-        @test g_new == g_new_new
-        @test length(reducer_new.region_list) == nv(g_new)
-        @test length(reducer_new_new.region_list) == nv(g_new_new)
     end
 end
